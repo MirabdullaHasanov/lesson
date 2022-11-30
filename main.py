49f928a965c23446186ff24b5dd730addad9dd4f
Гасанов Мирабдулла Эльчин <Gasa_ju52@itstep.edu.az>
2022-11-30 19:29:31 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+firstnumber = input("first number:")
+secondnumber = input("second number:")
+thirdnumber = input("third number:")
+
+firstnum = int(firstnumber)
+secondnum = int(secondnumber)
+thirdnum = int(thirdnumber)
+
+
+print(firstnum*100+secondnum*10+thirdnum*1)
